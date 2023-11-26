1c4ba2ee9e8b3970c48f789e85b49b906c52a37d
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-26 22:48:21 +0100

    deps with extract_package_dependencies

--- Development/Dependencies.R
+++ Development/Dependencies.R
@@ -1,3 +1,83 @@
+Dependency file generated on Sun Nov 26 22:47:53 2023 
+
+#################################################################################################### 
+DependencyTools.R
+#################################################################################################### 
+$`character(0)`
+[1] "format_node"  "get.edgelist" "write_clip"  
+
+$`package:base`
+ [1] "asNamespace"  "body"         "c"            "cat"         
+ [5] "character"    "deparse"      "Filter"       "get"         
+ [9] "gregexpr"     "grep"         "gsub"         "inherits"    
+[13] "is.character" "is.function"  "is.list"      "is.logical"  
+[17] "is.null"      "lapply"       "length"       "list"        
+[21] "ls"           "message"      "names"        "nchar"       
+[25] "nrow"         "paste"        "paste0"       "print"       
+[29] "regmatches"   "return"       "sapply"       "setdiff"     
+[33] "sprintf"      "stopifnot"    "tryCatch"     "unique"      
+[37] "unlist"      
+
+$`package:PackageTools`
+[1] ".count_and_print_function_summary" "analyze_function_dependencies"    
+[3] "get_package_functions"             "map_functions_to_packages"        
+
+$`package:stats`
+[1] "setNames"
+
+$`package:utils`
+[1] "browseURL"          "installed.packages"
+
+character(0)
+base
+PackageTools
+stats
+utils
+#################################################################################################### 
+DocumentationTools.R
+#################################################################################################### 
+$`c(".GlobalEnv", "package:PackageTools")`
+[1] "create_package"
+
+$`character(0)`
+[1] "create"                 "document"              
+[3] "gsub_file"              "isAvailable"           
+[5] "list.functions.in.file" "write_clip"            
+
+$`package:base`
+ [1] "basename"     "c"            "cat"          "date"        
+ [5] "dir.create"   "dir.exists"   "file.copy"    "file.exists" 
+ [9] "file.path"    "file.remove"  "gsub"         "is.character"
+[13] "list"         "list.files"   "names"        "paste"       
+[17] "paste0"       "print"        "rep"          "setwd"       
+[21] "sink"         "source"       "stop"         "stopifnot"   
+[25] "Sys.time"     "warnings"     "write"       
+
+$`package:PackageTools`
+[1] ".parse_description"    ".update_citation_file"
+
+$`package:utils`
+[1] "person"
+
+c(".GlobalEnv", "PackageTools")
+character(0)
+base
+PackageTools
+utils
+#################################################################################################### 
+Miscellaneous.R
+#################################################################################################### 
+$`character(0)`
+[1] "write_clip"
+
+$`package:base`
+[1] "c"      "paste0" "stop"  
+
+character(0)
+base
+#################################################################################################### 
+PackageTools.R
+#################################################################################################### 
 $`package:base`
  [1] "basename"     "c"            "cat"          "character"   
  [5] "close"        "dirname"      "file"         "file.exists" 
@@ -19,3 +99,29 @@ $`package:utils`
 base
 PackageTools
 utils
+#################################################################################################### 
+ReplacementTools.R
+#################################################################################################### 
+$`package:base`
+ [1] "c"            "file.exists"  "gsub"         "invisible"   
+ [5] "is.character" "length"       "paste0"       "readLines"   
+ [9] "return"       "sapply"       "stopifnot"    "warning"     
+[13] "writeLines"  
+
+base
+#################################################################################################### 
+RoxygenTools.R
+#################################################################################################### 
+$`package:base`
+ [1] "append"       "c"            "file.exists"  "grep"        
+ [5] "grepl"        "is.character" "is.list"      "lapply"      
+ [9] "length"       "list"         "max"          "paste"       
+[13] "paste0"       "readLines"    "regexpr"      "regmatches"  
+[17] "return"       "stopifnot"    "strsplit"     "unique"      
+[21] "writeLines"  
+
+$`package:PackageTools`
+[1] "add_import_from"     "find_package_calls"  "get_function_bodies"
+
+base
+PackageTools
