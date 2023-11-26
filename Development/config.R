d22a1b254d10db588e793dfdcb15cf8631c2f38b
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-26 21:37:15 +0100

    config

---
+++ Development/config.R
@@ -0,0 +1,15 @@
+# Configuration for the Package
+DESCRIPTION <- list(
+  package.name = "PackageTools",
+  version = "0.3.0",
+  title = "PackageTools - Help for R package development.",
+  description = "PackageTools is a collection of utility functions for code base statistics & and dependencies.",
+
+  author.given = "Abel",
+  author.family = "Vertesy",
+  author.email = "av@imba.oeaw.ac.at",
+  license = "GPL-3 + file LICENSE",
+  depends = "base",
+  imports = "checkmate",
+  github.user = "vertesy"
+)
