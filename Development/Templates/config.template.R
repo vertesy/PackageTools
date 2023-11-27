35d0fcc0a74629c286a8a80475263238ecae3bf2
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 10:44:17 +0100

    template

---
+++ Development/Templates/config.template.R
@@ -0,0 +1,19 @@
+# Configuration for the Package
+DESCRIPTION <- list(
+  package.name = "",
+  version = "",
+  title = ,
+  description = ,
+
+  depends = "",
+  imports = "",
+  suggests = "",
+
+  author.given = "Abel",
+  author.family = "Vertesy",
+  author.email = "av@imba.oeaw.ac.at",
+  github.user = "vertesy",
+  license = "GPL-3 + file LICENSE"
+)
+
+
