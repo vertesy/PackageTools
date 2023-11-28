86d94326923d274cb354735aa8ab74298994082d
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-28 15:11:35 +0100

    templates

--- Templates/Development/Create_the_PackageTools_Package.OLD.R
+++ Templates/Development/Create_the_PackageTools_Package.OLD.R
@@ -30,16 +30,16 @@ package.FnP <-		paste0(RepositoryDir, "R/", fname)
 BackupDir <- "~/GitHub/Packages/PackageTools/Development/"
 dir.create(BackupDir)
 
-DESCRIPTION <- list("Title" = "PackageTools - Help for R package development."
+DESCRIPTION <- list("Title" = "PackageTools - ..."
                     , "Author" = person(given = "Abel", family = "Vertesy", email = "av@imba.oeaw.ac.at", role =  c("aut", "cre") )
                     , "Authors@R" = 'person(given = "Abel", family = "Vertesy", email = "av@imba.oeaw.ac.at", role =  c("aut", "cre") )'
-                    , "Description" = "PackageTools is a collection of utility functions for code base statistics & and dependencies."
+                    , "Description" = "PackageTools is a set of R tools for ...."
                     , "License" = "GPL-3 + file LICENSE"
                     , "Version" = package.version
                     , "Packaged" =  Sys.time()
                     # , "Repository" =  "CRAN"
-                    , "Depends" =  "base"
-                    , "Imports" = "checkmate"
+                    , "Depends" =  ""
+                    , "Imports" = ""
                     # , "Suggests" = ""
                     , "BugReports"= "https://github.com/vertesy/PackageTools/issues"
 )
